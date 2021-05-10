44c41e8adf4fbedac400311d0b6fdb53b6c44b57
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-05-10 20:16:10 +0200

    feat: triangle (oneliner)

--- python/triangle.py
+++ python/triangle.py
@@ -2,8 +2,8 @@ import sys
 
 size = int(sys.stdin.readline().rstrip('\n'))
 
+# iterative answer (dummy)
 answer = ""
-
 for middle in range(1, size+1):
     i = 1
     while i < middle:
@@ -14,6 +14,9 @@ for middle in range(1, size+1):
         i -= 1
     answer += "\n"
 print(answer, end="")
+
+# onliner answer
+print("\n".join(["".join(["{}".format(y) for y in range(1, x+1)] + ["{}".format(y) for y in range(x-1, 0, -1)]) for x in range(1, size+1)]))
 
 # only one print statement is allowed !
 # str() is not allowed
