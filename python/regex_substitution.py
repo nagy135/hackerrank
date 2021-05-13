afcb213a27b8c24b41c4c386e9ab6ccfd60b1778
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-05-13 18:40:27 +0200

    feat: regex_substitution

---
+++ python/regex_substitution.py
@@ -0,0 +1,11 @@
+import sys
+import re
+
+
+sys.stdin.readline() # we dont need first line
+
+lines = list(map(lambda x: x.rstrip('\n'), sys.stdin.readlines()))
+lines = list(map(lambda x: re.sub(r' && ', ' and ', x), lines))
+lines = list(map(lambda x: re.sub(r' \|\| ', ' or ', x), lines))
+
+print("\n".join(lines))
