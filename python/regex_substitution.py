b179d5ef827459d04450a6c3757fc9f3d59a05c0
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-05-13 18:46:38 +0200

    feat: regex_substitution test cases

--- python/regex_substitution.py
+++ python/regex_substitution.py
@@ -10,3 +10,36 @@ lines = list(map(lambda x: re.sub(r' \|\| ', ' or ', x), lines))
 lines = list(map(lambda x: re.sub(r'  ', ' ', x), lines))
 
 print("\n".join(lines))
+
+# ❯ python regex_substitution.py <<EOF
+# 11
+# a = 1;
+# b = input();
+# 
+# if a + b > 0 && a - b < 0:
+#     start()
+# elif a*b > 10 || a/b < 1:
+#     stop()
+# print set(list(a)) | set(list(b))
+# #Note do not change &&& or ||| or & or |
+# #Only change those '&&' which have space on both sides.
+# #Only change those '|| which have space on both sides.
+# EOF              <---- answer is after this
+# a = 1;
+# b = input();
+# 
+# if a + b > 0 and a - b < 0:
+#     start()
+# elif a*b > 10 or a/b < 1:
+#     stop()
+# print set(list(a)) | set(list(b))
+# #Note do not change &&& or ||| or & or |
+# #Only change those '&&' which have space on both sides.
+# #Only change those '|| which have space on both sides.
+
+
+# ❯ python regex_substitution.py <<EOF
+# 1
+# x&& &&& && && x || | ||\|| x
+# EOF               <---- answer after this
+# x&& &&& and and x or | ||\|| x
