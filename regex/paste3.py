5953ca595caa60d53b65176f661193c55eb45d7e
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-05-15 21:10:59 +0200

    fix: paste3 example

--- regex/paste3.py
+++ regex/paste3.py
@@ -3,3 +3,26 @@ import sys
 
 lines = re.sub(r'\n', '\t', sys.stdin.read())
 print(lines)
+
+
+# ❯ python paste3.py <<EOF
+# Albany, N.Y.
+# Albuquerque, N.M.
+# Anchorage, Alaska
+# Asheville, N.C.
+# Atlanta, Ga.
+# Atlantic City, N.J.
+# Austin, Texas
+# Baltimore, Md.
+# Baton Rouge, La.
+# Billings, Mont.
+# Birmingham, Ala.
+# Bismarck, N.D.
+# Boise, Idaho
+# Boston, Mass.
+# Bridgeport, Conn.
+# EOF
+# Albany, N.Y.    Albuquerque, N.M.       Anchorage, Alaska       Asheville, N.C. Atlanta, Ga. A
+# tlantic City, N.J.      Austin, Texas   Baltimore, Md.  Baton Rouge, La.        Billings, Mont
+# .       Birmingham, Ala.        Bismarck, N.D.  Boise, Idaho    Boston, Mass.   Bridgeport, Co
+# nn.
